4788815c620f8c1468259ba261ee44dc95762536
Sushant <chavansushant794@gmail.com>
2024-02-07 12:11:07 +0530

    First commit

---
+++ src/data/home.ts
@@ -0,0 +1,333 @@
+export const categories = [
+  "All",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Computer programming",
+  "AI",
+  "Live",
+  "Stocks",
+  "Frontend Web Development",
+  "Weight Training",
+  "Psychology",
+  "History",
+  "Backend Web Development",
+  "Web Development",
+  "Podcasts",
+];
+
+export const videos = [
+  {
+    id: "1",
+    title: "Marvel Studios' Avengers: Endgame - Official Trailer",
+    channel: {
+      name: "Marvel Entertainment",
+      id: "Marvel Entertainment",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ugAmG9LeliJJoiyacIecdiq_ZgRNdmjCIohaN5x3QEOmWB9dNUsKuCU8ngLs3JUauHZ4-boVkA=s800-c-k-c0x00ffffff-no-rj",
+    },
+    views: 103400000,
+    postedAt: new Date("2020-03-03"),
+    duration: 126,
+    thumbnailUrl: "https://i3.ytimg.com/vi/TcMBFSGVi1c/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  },
+
+
+  {
+    id: "2",
+    title: "Front-end developer takes on a CSS battle",
+    channel: {
+      name: "Kevin Powell",
+      id: "KevinPowell",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APkrFKa6XiLa13mMVPzkmmTBcgNPjjqCGPrY86KfJFmf5w=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 1232300,
+    postedAt: new Date("2023-10-05"),
+    duration: 120,
+    thumbnailUrl: "https://i.ytimg.com/vi/eYPyIq5Y3Rk/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "3",
+    title: "CSS Anchor Is The Best New CSS Feature Since Flexbox",
+    channel: {
+      name: "Web Dev Simplified",
+      id: "WebDevSimplified",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 222536,
+    postedAt: new Date("2023-08-29"),
+    duration: 938,
+    thumbnailUrl: "https://i.ytimg.com/vi/B4Y9Ed4lLAI/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "4",
+    title: "Quick guide to CSS focus states",
+    channel: {
+      name: "Kevin Powell",
+      id: "KevinPowell",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APkrFKa6XiLa13mMVPzkmmTBcgNPjjqCGPrY86KfJFmf5w=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 112,
+    postedAt: new Date("2023-9-19"),
+    duration: 4343,
+    thumbnailUrl: "https://i.ytimg.com/vi/apdD69J4bEc/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+
+  {
+    id: "5",
+    title: "How to get ahead of 99% of people",
+    channel: {
+      name: "Ruri Ohama",
+      id: "Ruri Ohama",
+      profileUrl:
+        "https://yt3.googleusercontent.com/gw6BRWav3SyG39C2kmEM1VSb5ocjEWuKRBKPhRndqOKmAxj3rzB5OOVQKeE0751DJrPWJH7c=s900-c-k-c0x00ffffff-no-rj",
+    },
+    views: 777777,
+    postedAt: new Date("2023-9-08"),
+    duration: 1117,
+    thumbnailUrl: "https://i3.ytimg.com/vi/p_0MaEUBMYg/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  },
+  {
+    id: "6",
+    title: "Learn React From Scratch in 1 hour",
+    channel: {
+      name: "Web Dev Simplified",
+      id: "Web Dev Simplified",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 1032330,
+    postedAt: new Date("2023-08-09"),
+    duration: 2781,
+    thumbnailUrl: "https://i3.ytimg.com/vi/c8-eYB--j-Q/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  },
+  {
+    id: "7",
+    title: "Sia - Unstoppable (Official Audio)",
+    channel: {
+      name: "Sia",
+      id: "Sia",
+      profileUrl:
+        "https://yt3.ggpht.com/4cFOo3R_D4gOVVJPEt8aoff-Fi2eRjM5Nxz5fDXpw0WS57go_buPi0kvhehvTnBZeF3qSmAT=s900-c-k-c0x00ffffff-no-nd-rj",
+    },
+    views: 172464814,
+    postedAt: new Date("2016-1-21"),
+    duration: 217,
+    thumbnailUrl: "https://i3.ytimg.com/vi/cxjvTXo9WWM/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  },
+  {
+    id: "8",
+    title: "THE BATMAN – Main Trailer",
+    channel: {
+      name: "Warner Bros. Pictures",
+      id: "Warner Bros. Pictures",
+      profileUrl:
+        "https://yt3.googleusercontent.com/CoYiDB8ojHTQvSwr9h8clzpcm85wiFaHroW0MaVgTnDEXAIMxhGlzC5Vwo9N3-z3obfVqc_G62I=s900-c-k-c0x00ffffff-no-rj",
+    },
+    views: 60000000,
+    postedAt: new Date("2021-11-02"),
+    duration: 1000,
+    thumbnailUrl: "https://i3.ytimg.com/vi/mqqft2x_Aa4/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  },
+  {
+    id: "9",
+    title: "I Cannot Believe React Made A Hook For This",
+    channel: {
+      name: "Web Dev Simplified",
+      id: "WebDevSimplified",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 42345,
+    postedAt: new Date("2023-03-02"),
+    duration: 1000,
+    thumbnailUrl: "https://i.ytimg.com/vi/M3mGY0pgFk0/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "10",
+    title: "I Got Laid Off...",
+    channel: {
+      name: "Caleb Curry",
+      id: "CalebCurry",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ytc/APkrFKbpSojje_-tkBQecNtFuPdSCrg3ZT0FhaYjln9k0g=s176-c-k-c0x00ffffff-no-rj",
+    },
+    views: 10340,
+    postedAt: new Date("2023-03-03"),
+    duration: 54,
+    thumbnailUrl: "https://i.ytimg.com/vi/i2JVQdLnkAY/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "11",
+    title: "Tails OS in 100 Seconds",
+    channel: {
+      name: "Fireship",
+      id: "Fireship",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+    },
+    views: 10323340,
+    postedAt: new Date("2023-08-09"),
+    duration: 100,
+    thumbnailUrl: "https://i.ytimg.com/vi/mVKAyw0xqxw/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "12",
+    title: "Zig in 100 Seconds",
+    channel: {
+      name: "Fireship",
+      id: "Fireship",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/kxT8-C1vmd4/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "13",
+    title: "Peaky Blinders Season 6 Official Trailer | Netflix India",
+    channel: {
+      name: "Netflix India",
+      id: "Netflix India",
+      profileUrl:
+        "https://yt3.googleusercontent.com/zgMN9BuSQByG1SrpmLwcNB3MQhjDhS_pl9H1h7TaRievMfS4UpU7Z36j77z5_hnIW4N8uFX3NA=s900-c-k-c0x00ffffff-no-rj",
+    },
+    views: 5323,
+    postedAt: new Date("2023-8-17"),
+    duration: 93,
+    thumbnailUrl: "https://i3.ytimg.com/vi/PxZ5gGfPtCQ/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  },
+  {
+    id: "14",
+    title: "Marvel Studios' Avengers: Endgame - Official Trailer",
+    channel: {
+      name: "Marvel Entertainment",
+      id: "Marvel Entertainment",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ugAmG9LeliJJoiyacIecdiq_ZgRNdmjCIohaN5x3QEOmWB9dNUsKuCU8ngLs3JUauHZ4-boVkA=s800-c-k-c0x00ffffff-no-rj",
+    },
+    views: 103400000,
+    postedAt: new Date("2020-03-03"),
+    duration: 126,
+    thumbnailUrl: "https://i3.ytimg.com/vi/TcMBFSGVi1c/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  },
+
+  {
+    id: "15",
+    title: "Hidden Details in Loki's Costumes! | Behind the Scenes",
+    channel: {
+        name: "Marvel Entertainment",
+        id: "Marvel Entertainment",
+        profileUrl:
+            "https://yt3.googleusercontent.com/ugAmG9LeliJJoiyacIecdiq_ZgRNdmjCIohaN5x3QEOmWB9dNUsKuCU8ngLs3JUauHZ4-boVkA=s800-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-11-17"),
+    duration: 307,
+    thumbnailUrl: "https://i3.ytimg.com/vi/mS9nfsA-oGI/maxresdefault.jpg",
+    videoUrl:
+        "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+},
+{
+  id: "16",
+  title: "Learn useState Hook in React JS | React Hooks Explained",
+  channel: {
+      name: "Web Dev Simplified",
+      id: "Web Dev Simplified",
+      profileUrl:
+          "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+  },
+  views: 30323,
+  postedAt: new Date("2023-9-17"),
+  duration: 990,
+  thumbnailUrl: "https://i3.ytimg.com/vi/z_MnNejwR8s/maxresdefault.jpg",
+  videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+},
+{
+  id: "17",
+  title: "Magic Navigation Menu Indicator Using HTML CSS JavaScript",
+  channel: {
+      name: "Web Dev Simplified",
+      id: "Web Dev Simplified",
+      profileUrl:
+          "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+  },
+  views: 8323,
+  postedAt: new Date("2023-11-15"),
+  duration: 1113,
+  thumbnailUrl: "https://i3.ytimg.com/vi/ZCpQm7Qn03k/maxresdefault.jpg",
+  videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+},
+
+
+{
+  id: "18",
+  title: "Spotify Clone Using HTML CSS | Creating a Spotify Clone",
+  channel: {
+      name: "Fireship",
+      id: "Fireship",
+      profileUrl:
+          "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+  },
+  views: 888000,
+  postedAt: new Date("2023-09-29"),
+  duration: 2586,
+  thumbnailUrl: "https://i3.ytimg.com/vi/TdjmTjuU8JE/maxresdefault.jpg",
+  videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+},
+
+
+
+{
+  id: "19",
+  title: "NEW Way To Create Variables In JavaScript",
+  channel: {
+    name: "Web Dev Simplified",
+    id: "WebDevSimplified",
+    profileUrl:
+      "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+  },
+  views: 257136,
+  postedAt: new Date("2023-08-22"),
+  duration: 732,
+  thumbnailUrl: "https://i.ytimg.com/vi/d6a8RymS1zI/maxresdefault.jpg",
+  videoUrl:
+    "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+},
+];
---
+++ src/data/sidebar.ts
@@ -0,0 +1,72 @@
+export const subscriptions = [
+  {
+    channelName: "Fireship",
+    id: "Fireship",
+    imgUrl:
+      "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "Caleb Curry",
+    id: "CalebCurry",
+    imgUrl:
+      "https://yt3.googleusercontent.com/ytc/APkrFKbpSojje_-tkBQecNtFuPdSCrg3ZT0FhaYjln9k0g=s176-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "Web Dev Simplified",
+    id: "WebDevSimplified",
+    imgUrl:
+      "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "Kevin Powell",
+    id: "KevinPowell",
+    imgUrl:
+      "https://yt3.ggpht.com/ytc/APkrFKa6XiLa13mMVPzkmmTBcgNPjjqCGPrY86KfJFmf5w=s48-c-k-c0x00ffffff-no-rj",
+  },
+  
+  {
+    channelName: "Marvel Entertainment",
+    id: "Marvel Entertainment",
+    imgUrl:
+        "https://yt3.googleusercontent.com/ugAmG9LeliJJoiyacIecdiq_ZgRNdmjCIohaN5x3QEOmWB9dNUsKuCU8ngLs3JUauHZ4-boVkA=s800-c-k-c0x00ffffff-no-rj",
+},
+{
+  channelName: "Sia",
+  id: "Sia",
+  imgUrl:
+      "https://yt3.ggpht.com/4cFOo3R_D4gOVVJPEt8aoff-Fi2eRjM5Nxz5fDXpw0WS57go_buPi0kvhehvTnBZeF3qSmAT=s900-c-k-c0x00ffffff-no-nd-rj",
+},
+{
+  channelName: "Ruri Ohama",
+  id: "Ruri Ohama",
+  imgUrl:
+      "https://yt3.googleusercontent.com/gw6BRWav3SyG39C2kmEM1VSb5ocjEWuKRBKPhRndqOKmAxj3rzB5OOVQKeE0751DJrPWJH7c=s900-c-k-c0x00ffffff-no-rj",
+},
+{
+  channelName: "Warner Bros. Pictures",
+  id: "Warner Bros. Pictures",
+  imgUrl:
+      "https://yt3.googleusercontent.com/CoYiDB8ojHTQvSwr9h8clzpcm85wiFaHroW0MaVgTnDEXAIMxhGlzC5Vwo9N3-z3obfVqc_G62I=s900-c-k-c0x00ffffff-no-rj",
+},
+  {
+    channelName: "Traversy Media",
+    id: "TraversyMedia",
+    imgUrl:
+      "https://yt3.googleusercontent.com/ytc/APkrFKYcYswt_UhD7D0j6ddiQz6Gb8Q_vSJOjhYI0CoXSw=s176-c-k-c0x00ffffff-no-rj-mo",
+  },
+
+  {
+    channelName: "Netflix India",
+    id: "Netflix India",
+    imgUrl:
+        "https://yt3.googleusercontent.com/zgMN9BuSQByG1SrpmLwcNB3MQhjDhS_pl9H1h7TaRievMfS4UpU7Z36j77z5_hnIW4N8uFX3NA=s900-c-k-c0x00ffffff-no-rj",
+},
+]
+
+export const playlists = [
+  { id: "1", name: "Next.JS" },
+  { id: "2", name: "Favorites" },
+  { id: "3", name: "React Hooks" },
+  { id: "5", name: "TypeScript" },
+  { id: "4", name: "Human psychology" },
+]
